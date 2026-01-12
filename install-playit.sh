e6caa8fa90d78e03e705b550712ff0e17e838459
ZarkosX <96589192+ZarkosX@users.noreply.github.com>
2026-01-12 23:32:08 +0200

    Update install-playit.sh

--- install-playit.sh
+++ install-playit.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-# --------------------------------------
-# 📜 Fox Script
-# 🔒 Copyright (c) 2025 Fox - All Rights Reserved
-# 📌 This script is created and maintained by Fox (aka Adolta)
-# 🌐 https://github.com/Omar9282jwee/Fox-scripts
-# --------------------------------------
-
-echo "🔄 Fox Script is starting..."
+echo "🔄 Playit.gg Script is starting..."
 
 # Download playit agent
 wget https://github.com/playit-cloud/playit-agent/releases/download/v0.15.26/playit-linux-amd64
@@ -18,4 +11,4 @@ chmod +x playit-linux-amd64
 # Run the agent
 ./playit-linux-amd64
 
-echo "✅ Fox Script finished."
+echo "✅ Playit.gg Script finished."
